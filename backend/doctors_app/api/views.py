91b56c3a868ff1de4aec1abb22cf8b6de3bf5306
“G-Gakii” <gakiiviolet1@gmail.com>
2025-05-25 15:04:55 +0300

    only doctor or admin can delete a doctor or update doctor details

--- backend/doctors_app/api/views.py
+++ backend/doctors_app/api/views.py
@@ -1,6 +1,9 @@
 from doctors_app.models import Doctor
 from doctors_app.api.serializers import DoctorSerializer
 from rest_framework import generics
+from doctors_app.api.permissions import IsOwnerOrAdmin
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class DoctorList(generics.ListCreateAPIView):
@@ -9,5 +12,6 @@ class DoctorList(generics.ListCreateAPIView):
 
 
 class DoctorDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
     queryset = Doctor.objects.all()
     serializer_class =DoctorSerializer
